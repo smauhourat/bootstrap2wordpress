f745c2eb24a5550a79282d9669f7d56436a35416
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-09-21 15:33:41 -0300

    banner paret

--- index.php
+++ index.php
@@ -1,39 +1,7 @@
 <?php 
   get_header();
+  get_template_part('template-parts/banner', 'title');
 ?>
-<section class="title-banner">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 offset-md-1 col-sm-12 offset-sm-0 overflow-hidden text-center">
-          <p class="tag-line sub-title">Bootstrap to WordPress Course 2.0</p>
-          <h1 class="page-title">Blog</h1>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="subscribe-bar">
-    <div class="container">
-      <div class="row flex-vertical-center">
-        <div class="col-sm-6">
-          <p><strong>Want to save 20% on the course?</strong> Enter your email and we'll send you the discount code!</p>
-        </div>
-        <div class="col-sm-6">
-          <form action="index.html" class="" method="post">
-          	<div class="row">
-          		<div class="col-lg-8">
-          			<input name="test" type="text" value="">
-          		</div>
-          		<div class="col-lg-4">
-          			<button class="btn btn-invert m-0" name="button" type="button">Subscribe</button>
-          		</div>
-          	</div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
-
   <div class="content-area">
     <div class="container">
       <div class="row">
