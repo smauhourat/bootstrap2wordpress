ce44009cdf95be38c13d277dacd4b5ce5ee53e96
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-09-21 13:05:45 -0300

    initial

---
+++ functions.php
@@ -0,0 +1,50 @@
+<?php
+
+/*
+    Enqueue scripts and styles
+*/
+
+/**
+ * Undocumented function
+ *
+ * @return void
+ */
+
+if ( !function_exists( 'b2w_theme_setup' ) ) {
+    /* Theme Setup */
+    function b2w_theme_setup() {
+        load_theme_textdomain('bootstrap2wordpress', get_template_directory() . '/languages');
+        add_theme_support( 'title-tag' );
+        add_theme_support( 'post-thumbnails' );
+        add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+        add_theme_support( 'customize-selective-refresh-widgets' );
+        add_theme_support( 'responsive/embeds' );
+
+        register_nav_menus(array(
+            'primary' => esc_html__( 'Primary Menu', 'bootstrap2wordpress' )
+        ));
+    }
+
+    add_action('after_setup_theme', 'b2w_theme_setup' );
+}
+
+function b2w_assets() {
+
+    // Enqueue CSS Files
+
+    wp_enqueue_style('google-font', '//fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap', array(), '1.0', 'all');
+    wp_enqueue_style('bootstrap', get_theme_file_uri('assets/css/bootstrap.min.css'), array(), 'v5.1.1', 'all');
+    wp_enqueue_style('flaticon', get_theme_file_uri('assets/font/flaticon.css'), array(), false, 'all');
+    wp_enqueue_style('bootstrap2wordpress', get_stylesheet_uri(), array('bootstrap'), '1.0', 'all');
+
+    // Enqueue JS Files
+    wp_enqueue_script('bootstrap', get_theme_file_uri('assets/js/bootstrap.min.js'), array(), 'v5.1.1', true);
+    // wp_enqueue_script('bootstrap2wordpress-js', get_theme_file_uri('assets/js/main-script.js'), array('jquery', 'jquery-ui-core', 'jquery-ui-select-menu'), '1.0', true);
+    wp_enqueue_script('bootstrap2wordpress-js', get_theme_file_uri('assets/js/main-script.js'), array('jquery'), '1.0.0', true);
+
+    if ( is_singular() && comments_open($post_id) && get_option('thread_comments') ) {
+        wp_enqueue_script('comment-reply');
+    }
+}
+
+add_action('wp_enqueue_scripts', 'b2w_assets');
---
+++ index.php
@@ -0,0 +1,93 @@
+<?php 
+  get_header();
+?>
+<section class="title-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 offset-md-1 col-sm-12 offset-sm-0 overflow-hidden text-center">
+          <p class="tag-line sub-title">Bootstrap to WordPress Course 2.0</p>
+          <h1 class="page-title">Blog</h1>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="subscribe-bar">
+    <div class="container">
+      <div class="row flex-vertical-center">
+        <div class="col-sm-6">
+          <p><strong>Want to save 20% on the course?</strong> Enter your email and we'll send you the discount code!</p>
+        </div>
+        <div class="col-sm-6">
+          <form action="index.html" class="" method="post">
+          	<div class="row">
+          		<div class="col-lg-8">
+          			<input name="test" type="text" value="">
+          		</div>
+          		<div class="col-lg-4">
+          			<button class="btn btn-invert m-0" name="button" type="button">Subscribe</button>
+          		</div>
+          	</div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <div class="content-area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 offset-md-2 overflow-hidden">
+
+          <article class="post">
+            <div class="meta">
+              <span>Sep 24, 2021</span>
+            </div>
+            <h2><a href="#">Blog post title</a></h2>
+            <p class="excerpt">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
+            </p>
+            <a href="#" class="read-more">Read the full post -&gt;</a>
+          </article>
+
+          <article class="post my-5">
+            <div class="meta">
+              <span>Sep 24, 2021</span>
+            </div>
+            <h2><a href="#">Blog post title</a></h2>
+            <p class="excerpt">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
+            </p>
+            <a href="#" class="read-more">Read the full post -&gt;</a>
+          </article>
+
+          <article class="post my-5">
+            <div class="meta">
+              <span>Sep 24, 2021</span>
+            </div>
+            <h2><a href="#">Blog post title</a></h2>
+            <p class="excerpt">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
+            </p>
+            <a href="#" class="read-more">Read the full post -&gt;</a>
+          </article>
+
+          <article class="post my-5">
+            <div class="meta">
+              <span>Sep 24, 2021</span>
+            </div>
+            <h2><a href="#">Blog post title</a></h2>
+            <p class="excerpt">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores veritatis expedita molestias id, dolorem sapiente quae dolores modi hic et ea, suscipit minima eligendi magnam odio reiciendis dolorum, non placeat?
+            </p>
+            <a href="#" class="read-more">Read the full post -&gt;</a>
+          </article>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php 
+  get_footer();
+?>
